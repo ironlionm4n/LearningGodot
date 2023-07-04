b71293e4d28ce5d2d36a83439f19387ee6f9d9d9
Mikail Miller <65094447+ironlionm4n@users.noreply.github.com>
2023-07-04 08:57:15 -0400

    clean up level script

--- Scripts/Level.cs
+++ Scripts/Level.cs
@@ -5,18 +5,6 @@ namespace LearningGodot.Scripts;
 
 	public partial class Level : Node2D
 	{
-
-		// Called when the node enters the scene tree for the first time.
-		public override void _Ready()
-		{
-		
-		}
-
-		// Called every frame. 'delta' is the elapsed time since the previous frame.
-		public override void _Process(double delta)
-		{
-		}
-
 		private void OnArea2DBodyExited(Node2D body)
 		{
 			var sceneTree = GetTree();
